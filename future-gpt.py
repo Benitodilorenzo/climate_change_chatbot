c737ae0e8b44ed7692404ad39df3d7880945a100
Olav Benjamin di Lorenzo <50009241+Benitodilorenzo@users.noreply.github.com>
2023-07-07 15:22:15 +0200

    Update future-gpt.py

--- future-gpt.py
+++ future-gpt.py
@@ -24,7 +24,10 @@ st.title('Climate Change Chatbot')
 # User inputs their idea
 user_idea_content = st.text_input('Enter your idea here:')
 
-future_gpt_response = ''
+# Initialize session_state if it doesn't exist
+if 'future_gpt_response' not in st.session_state:
+    st.session_state.future_gpt_response = ''
+
 # Generate Future Scenario button
 if st.button('Generate Future Scenario'):
     if user_idea_content:
@@ -36,18 +39,18 @@ if st.button('Generate Future Scenario'):
             messages=[future_gpt_prompt, user_idea],
         )
 
-        future_gpt_response = response['choices'][0]['message']['content']
-        st.write(future_gpt_response)
+        st.session_state.future_gpt_response = response['choices'][0]['message']['content']
+        st.write(st.session_state.future_gpt_response)
     else:
         st.write('Please enter your idea.')
 
 # Summarize and Visualize button
 if st.button('Summarize and Visualize'):
-    if future_gpt_response:
+    if st.session_state.future_gpt_response:
         # Generate a summarization prompt
         summarization_prompt = {
             "role": "system",
-            "content": "You are a helpful assistant. Summarize the following text: " + future_gpt_response
+            "content": "You are a helpful assistant. Summarize the following text: " + st.session_state.future_gpt_response
         }
 
         # Generate a response from GPT-3
