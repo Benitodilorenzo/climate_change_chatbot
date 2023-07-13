611bfdd8d954609162ebcb50b5839bf8c5cb4644
Olav Benjamin di Lorenzo <50009241+Benitodilorenzo@users.noreply.github.com>
2023-07-13 17:57:57 +0200

    Update game_guide.py

--- game_guide.py
+++ game_guide.py
@@ -58,35 +58,36 @@ def guide_gpt_conversation(user_inputs):
         messages=messages,
     )
 
-    guide_responses.extend([msg['message']['content']['text'] for msg in response['choices'] if 'message' in msg and 'role' in msg and msg['role'] == 'system'])
+    guide_responses = [msg['content']['text'] for msg in response['choices'] if 'content' in msg['message']]
+    return guide_responses
 
 
 
 
 guide_responses = []
 
-# Main function to run the interactive user journey
 def run_game():
     display_guide_image()  # Display the guide image initially
     choice = guide_initial_message()  # Ask the user to make a choice
 
     if choice == "Yes, I will enter.":
         user_inputs = ["The user has decided to enter the room."]  # Send the user's choice as the first input to Guide-GPT
-        guide_gpt_conversation(user_inputs)
+        guide_responses = guide_gpt_conversation(user_inputs)
         for guide_response in guide_responses:
             st.write("Guide:", guide_response)
 
         display_room_image()  # Display the room image
         user_inputs = st.text_input("You: ", key="user_input", value="", help="Type your message here").split('\n')
-        guide_gpt_conversation(user_inputs)
+        guide_responses = guide_gpt_conversation(user_inputs)
         for guide_response in guide_responses:
             st.write("Guide:", guide_response)
 
     elif choice == "No, I am not ready yet.":
         user_inputs = ["The user has decided not to enter the room."]  # Send the user's choice as the first input to Guide-GPT
-        guide_gpt_conversation(user_inputs)
+        guide_responses = guide_gpt_conversation(user_inputs)
         for guide_response in guide_responses:
             st.write("Guide:", guide_response)
+
 
 # Run the game
 if __name__ == "__main__":
