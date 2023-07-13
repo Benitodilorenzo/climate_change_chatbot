e82194269d6028f06fa0cb0cbb863af77c7fa90e
Olav Benjamin di Lorenzo <50009241+Benitodilorenzo@users.noreply.github.com>
2023-07-13 17:20:49 +0200

    Update game_guide.py

--- game_guide.py
+++ game_guide.py
@@ -59,8 +59,9 @@ def guide_gpt_conversation(user_inputs):
         messages=messages,
     )
 
-    guide_responses = [msg['message']['content'] for msg in response['choices'] if msg['role'] == 'system']
+    guide_responses = [msg['content']['text'] for msg in response['choices'] if msg['role'] == 'system']
     return guide_responses
+
 
 # Main function to run the interactive user journey
 def run_game():
